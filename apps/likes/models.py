bc401e835fc35410b32484bfa5f955abac570067
nuranov04 <artnyr2004@gmail.com>
2022-11-06 17:48:50 +0600

    create limit for likes

--- apps/likes/models.py
+++ apps/likes/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.response import Response
 
 from apps.posts.models import Post
+
 
 User = get_user_model()
 
@@ -25,3 +29,7 @@ class Like(models.Model):
 
     def __str__(self):
         return f"{self.post.id} --- {self.user.username_telegram}"
+
+    def save(self, *args, **kwargs):
+        if Like.objects.filter(user=self.user.id_telegram, post=self.post.id).count() < 1:
+            return super().save(*args, **kwargs)
--- apps/likes/serializers.py
+++ apps/likes/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.response import Response
 
 from apps.likes.models import Like
 
@@ -7,3 +8,10 @@ class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
         fields = "__all__"
+
+    def create(self, validated_data):
+        print(validated_data["user"].id_telegram)
+        print(validated_data["post"].id)
+        if Like.objects.filter(user=validated_data["user"].id_telegram, post=validated_data["post"].id).count() < 1:
+            return Like.objects.create(**validated_data)
+        raise serializers.ValidationError({"error": "you cannot like again"})
